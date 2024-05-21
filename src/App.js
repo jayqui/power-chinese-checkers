d9184e9f5224c76dcc1a32f7b967d81bb1e42fd5
Jay Quigley <quigley.james.g@gmail.com>
2024-05-20 20:53:16 -0400

    Empty board

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+
+const squares = {};
+const mySquares = [
+  "A13",
+  "B12", "B14",
+  "C11", "C13", "C15",
+  "D10", "D12", "D14", "D16",
+  "E1", "E3", "E5", "E7", "E9", "E11", "E13", "E15", "E17", "E19", "E21", "E23", "E25",
+  "F2", "F4", "F6", "F8", "F10", "F12", "F14", "F16", "F18", "F20", "F22", "F24",
+  "G3", "G5", "G7", "G9", "G11", "G13", "G15", "G17", "G19", "G21", "G23",
+  "H4", "H6", "H8", "H10", "H12", "H14", "H16", "H18", "H20", "H22",
+  "I5", "I7", "I9", "I11", "I13", "I15", "I17", "I19", "I21",
+  "J4", "J6", "J8", "J10", "J12", "J14", "J16", "J18", "J20", "J22",
+  "K3", "K5", "K7", "K9", "K11", "K13", "K15", "K17", "K19", "K21", "K23",
+  "L2", "L4", "L6", "L8", "L10", "L12", "L14", "L16", "L18", "L20", "L22", "L24",
+  "M1", "M3", "M5", "M7", "M9", "M11", "M13", "M15", "M17", "M19", "M21", "M23", "M25",
+  "N10", "N12", "N14", "N16",
+  "O11", "O13", "O15",
+  "P12", "P14",
+  "Q13",
+]
+mySquares.forEach(sq => squares[sq] = []);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {[...Array(17).keys()].map((x) =>
+        (<div className="Row">
+            {[...Array(25).keys()].map((y) => (
+              <Square letter={String.fromCharCode('Q'.charCodeAt(0) - x) + (y + 1)} />
+            ))}
+        </div>))}
     </div>
   );
+}
+
+function Square({ letter }) {
+  const isBoardSquare = Object.keys(squares).includes(letter);
+  let className = "Square";
+  if (isBoardSquare) { className += " BoardSquare"; }
+
+  return(
+    <span className={className}>
+      {isBoardSquare && letter}
+    </span>
+  )
 }
 
 export default App;
