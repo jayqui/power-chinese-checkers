51bbc9f638287acc93bea3360a6f017e2b73d0fe
Jay Quigley <quigley.james.g@gmail.com>
2024-05-21 13:00:16 -0700

    underway on getLegalMoves

--- src/App.js
+++ src/App.js
@@ -25,39 +25,21 @@ const squareCoordinates = [
 squareCoordinates.forEach((sq) => {
   const squareObj = {
     marbleColor: null,
-    neighbors: [],
   }
-
-  const letter = sq[0];
-  const prevLetter = String.fromCharCode(letter.charCodeAt(0) - 1);
-  const nextLetter = String.fromCharCode(letter.charCodeAt(0) + 1);
-  const number = Number(sq.slice(1));
-  const prevNumber = number - 1;
-  const nextNumber = number + 1;
-
-  if (squareCoordinates.includes(prevLetter + prevNumber)) { squareObj.neighbors.push(prevLetter + prevNumber) }
-  if (squareCoordinates.includes(prevLetter + nextNumber)) { squareObj.neighbors.push(prevLetter + nextNumber) }
-  if (squareCoordinates.includes(letter + (number - 2))) { squareObj.neighbors.push(letter + (number - 2)) }
-  if (squareCoordinates.includes(letter + (number + 2))) { squareObj.neighbors.push(letter + (number + 2)) }
-  if (squareCoordinates.includes(nextLetter + prevNumber)) { squareObj.neighbors.push(nextLetter + prevNumber) }
-  if (squareCoordinates.includes(nextLetter + nextNumber)) { squareObj.neighbors.push(nextLetter + nextNumber) }
-
   squares[sq] = squareObj;
 });
 
 
-const BOTTOM_PLAYER_START_COORDINATES = ['A13', 'B12', 'B14', 'C11', 'C13', 'C15', 'D10', 'D12', 'D14', 'D16'];
+const BOTTOM_PLAYER_START_COORDINATES = ['A13', 'G11', 'G9', 'C11', 'C13', 'H8', 'H10', 'H12', 'D14', 'D16'];
 const TOP_PLAYER_START_COORDINATES = ['Q13', 'P12', 'P14', 'O11', 'O13', 'O15', 'N10', 'N12', 'N14', 'N16'];
 const TOP_LEFT_PLAYER_START_COORDINATES = ['M1', 'M3', 'M5', 'M7', 'L2', 'L4', 'L6', 'K3', 'K5', 'J4']
 const TOP_RIGHT_PLAYER_START_COORDINATES = ['M19', 'M21', 'M23', 'M25', 'L20', 'L22', 'L24', 'K21', 'K23', 'J22']
 const BOTTOM_LEFT_PLAYER_START_COORDINATES = ['E1', 'E3', 'E5', 'E7', 'F2', 'F4', 'F6', 'G3', 'G5', 'H4']
 const BOTTOM_RIGHT_PLAYER_START_COORDINATES = ['E19', 'E21', 'E23', 'E25', 'F20', 'F22', 'F24', 'G21', 'G23', 'H22']
-// const tempRedStartCoordinates = ["H14"]
-// const tempBlueStartCoordinates = ["I15"]
 
 function App() {
   // TODO: allow to be dynamically set as part of intro/setup sequence
-  const PLAYERS = ['red', 'blue', 'green', 'yellow', 'white', 'purple'];
+  const PLAYERS = ['red', 'blue', 'green', 'yellow', 'purple', 'white'];
   const [whoseTurn, setWhoseTurn] = useState(PLAYERS[0]);
   const [selectedSquare, setSelectedSquare] = useState(null);
 
@@ -68,8 +50,8 @@ function App() {
     TOP_RIGHT_PLAYER_START_COORDINATES,
     BOTTOM_LEFT_PLAYER_START_COORDINATES,
     BOTTOM_RIGHT_PLAYER_START_COORDINATES,
-  ].forEach((coordinateList, index) => {
-    coordinateList.forEach(c => squares[c].marbleColor = PLAYERS[index]);
+  ].forEach((coordinateList, inde) => {
+    coordinateList.forEach(c => squares[c].marbleColor = PLAYERS[inde]);
   })
 
   function handleSquareClick(squareIdentifier) {
@@ -90,7 +72,7 @@ function App() {
             {[...Array(25).keys()].map((squareNumber) => (
               <Square
                 key={squareNumber}
-                selectedSquare={selectedSquare} // TODO: introduce state handling, e.g. React Context
+                selectedSquare={selectedSquare} // TODO: introduce state handling, e.g. React Contet
                 handleSquareClick={handleSquareClick}
                 squareIdentifier={String.fromCharCode('Q'.charCodeAt(0) - squareLetter) + (squareNumber + 1)}
               />
@@ -100,18 +82,61 @@ function App() {
   );
 }
 
- // TODO: get selectedSquare from a state tree, e.g. a React Context
+// TODO: get selectedSquare from a state tree, e.g. a React Contet
+function getLegalMoves({ selectedSquare }) {
+  if (!selectedSquare) return [];
+
+  const legalMoves = [];
+
+  const letter = selectedSquare[0];
+  const number = Number(selectedSquare.slice(1));
+
+  const addToLegalMovesIfApplicable = (candidateSquare) => {
+    if (squareCoordinates.includes(candidateSquare)) {
+      let shouldInclude = false;
+      if (squares[candidateSquare].marbleColor === null) shouldInclude = true;
+      if (shouldInclude === true)legalMoves.push(candidateSquare);
+    }
+  }
+
+  // Left
+  const leftCandidate = letter + (number - 2);
+  addToLegalMovesIfApplicable(leftCandidate);
+  // Right
+  const rightCandidate = letter + (number + 2);
+  addToLegalMovesIfApplicable(rightCandidate);
+  // Left Backward
+  const prevLetter = String.fromCharCode(letter.charCodeAt(0) - 1);
+  const leftBackwardCandidate = prevLetter + (number - 1);
+  addToLegalMovesIfApplicable(leftBackwardCandidate);
+  // Right Backward
+  const rightBackwardCandidate = prevLetter + (number + 1);
+  addToLegalMovesIfApplicable(rightBackwardCandidate);
+  // Left Forward
+  const nextLetter = String.fromCharCode(letter.charCodeAt(0) + 1);
+  const leftForwardCandidate = nextLetter + (number - 1);
+  addToLegalMovesIfApplicable(leftForwardCandidate);
+
+  // Right Forward
+  const rightForwardCandidate = nextLetter + (number + 1);
+  addToLegalMovesIfApplicable(rightForwardCandidate);
+
+  return legalMoves;
+}
+
+ // TODO: get selectedSquare from a state tree, e.g. a React Contet
 function Square({ handleSquareClick, squareIdentifier, selectedSquare }) {
   const isSelected = squareIdentifier === selectedSquare;
   const squareObj = squares[squareIdentifier];
-  const selectedSquareObj = squares[selectedSquare];
   const isBoardSquare = Object.keys(squares).includes(squareIdentifier);
   let className = "Square";
   if (isBoardSquare) className += " BoardSquare";
   if (isSelected) className += " SelectedSquare";
   if (squareObj && squareObj.marbleColor !== null) className += ` SquareColor--${squareObj.marbleColor}`;
-  // TODO: fix logic — not all neighbors are legal moves!!
-  if (selectedSquareObj && selectedSquareObj.neighbors.includes(squareIdentifier)) className += " LegalMove";
+
+  const legalMoves = getLegalMoves({ selectedSquare });
+
+  if (legalMoves.includes(squareIdentifier)) className += " LegalMove";
 
   return(
     <span
